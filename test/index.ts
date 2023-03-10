256ec7c9cbc07e8f06d19a8e55450ffd4ac3eef4
changfeng <changfeng@gaoding.com>
2023-03-11 03:50:44 +0800

    ci: remove type check in lint-staged

--- test/index.ts
+++ test/index.ts
@@ -3,29 +3,21 @@ import path from 'node:path';
 import glob from 'glob';
 import Mocha from 'mocha';
 
-export function run(testsRoot: string, callback: (error: any, failures?: number) => void): void {
+export async function run(
+    testsRoot: string,
+    callback: (error: any, failures?: number) => void,
+): Promise<void> {
     const mocha = new Mocha({ color: true });
 
-    console.log(testsRoot);
-
-    glob('**/**.test.js', { cwd: testsRoot }, (error, files) => {
-        if (error) {
-            callback(error);
-            return;
-        }
-
-        // Add files to the test suite
+    try {
+        const files = await glob('**/**.test.js', { cwd: testsRoot });
         for (const f of files) {
             mocha.addFile(path.resolve(testsRoot, f));
         }
-
-        try {
-            // Run the mocha test
-            mocha.run((failures) => {
-                callback(null, failures);
-            });
-        } catch (error_) {
-            callback(error_);
-        }
-    });
+        mocha.run((failures) => {
+            callback(null, failures);
+        });
+    } catch (error) {
+        callback(error);
+    }
 }
